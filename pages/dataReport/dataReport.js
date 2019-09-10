95b3d2e4e66d2d3242e806028f655cc161a608e6
段慧军 <duanhuijun@hqins.cn>
2019-09-10 20:45:25 +0800

    招募首页修改，产品icon修改

--- pages/dataReport/dataReport.js
+++ pages/dataReport/dataReport.js
@@ -11,25 +11,7 @@ Page({
    */
   data: {
     dayScore: [],
-    underwriting: [],
-    "chartData": {
-      "series": [{
-        "name": "一班",
-        "data": 50
-      }, {
-        "name": "二班",
-        "data": 30
-      }, {
-        "name": "三班",
-        "data": 20
-      }, {
-        "name": "四班",
-        "data": 18
-      }, {
-        "name": "五班",
-        "data": 8
-      }]
-    }
+    underwriting: []
   },
 
   /**
